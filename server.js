c7b899642f0e9cf5c6c21edbd93af9b9832e0474
Tuan.LA <luonganhtuan1051999@gmail.com>
2020-08-04 10:17:01 +0700

    update server

--- server.js
+++ server.js
@@ -24,6 +24,43 @@ app.get('/webhook', function(req, res) {
   res.send('Error, wrong validation token');
 });
 
+app.post('/webhooh', function(req, res){
+  var entries = req.body.entry ;
+  for( var entry of entries){
+    var messaging = entry.messaging ;
+    for(var message of messaging){
+      var senderId = messaging.sender.id ;
+      if(message.message){
+         // If user send text
+         if (message.message.text) {
+          var text = message.message.text;
+          console.log(text); // In tin nhắn người dùng
+          sendMessage(senderId, "Tui là bot đây: " + text);
+        }
+      }
+    }
+  }
+  res.status(200).send("OK");
+});
+// Gửi thông tin tới REST API để trả lời
+function sendMessage(senderId, message) {
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: "EAAEmHQHz0R8BAFZBkQJFkvdlBAzAYI5lvVtUOVWg4ZBPkWBcrIZB8UmIa5gKiGpqSykWe8VGZAMSsfouy2yIEZAzSDoLZB0nZBxhFyfBSn5SZBZAC8xmtnuhXJRqqIF7ZBrF3glAtRvKiX0C62Sjd8H3kr8ZBw6MIbNx3NyibdZCw4plXAZDZD",
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: senderId
+      },
+      message: {
+        text: message
+      },
+    }
+  });
+}
+
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 
